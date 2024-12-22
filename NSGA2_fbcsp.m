246ea44ad5633c620996124d08107ee95fc305aa
SanazRezvani <124499661+SanazRezvani@users.noreply.github.com>
2024-12-22 22:43:23 +1100

    Update NSGA2_fbcsp.m

--- NSGA2_fbcsp.m
+++ NSGA2_fbcsp.m
@@ -249,7 +249,6 @@ datatrain2=data2(:,:,:);
 % mm = input(prompt)
 
 
-
 % mm=1;                               %No. of spatial filters for each class
 % mm=2; 
 % mm=3; 
@@ -292,7 +291,7 @@ for i=1:size(datatrain1,3)         %#al    %No. of loops = No. of trials
 %     featuretrain2(:,i)=var(y2');
       
 %     
-% %     % Second approach to feature extraction for variance
+%     % Second approach to feature extraction for variance
 % %     featuretrain_1(:,i)=log10(var(y1')/sum(var(y1')));
 % %     featuretrain_2(:,i)=log10(var(y2')/sum(var(y2')));
 %       
